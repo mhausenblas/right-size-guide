4f0a35f85396cfe5fd0e8790657de49b19950e84
Michael Hausenblas <hausenbl@amazon.com>
2020-02-14 15:51:45 -0800

    refactors assessment logic, inits peak
    Signed-off-by: Michael Hausenblas <hausenbl@amazon.com>

--- main.go
+++ main.go
@@ -2,53 +2,114 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"os/exec"
 	"syscall"
 	"time"
 )
 
+// Findings captures idle and peak resource usage
+type Findings struct {
+	MemoryMaxRSS int64
+	CPUuser      syscall.Timeval
+	CPUsys       syscall.Timeval
+}
+
+var icmd, pcmd *exec.Cmd
+var idlef, peakf chan Findings
+
 func main() {
+	// Define the CLI API:
 	target := flag.String("target", "", "The filesystem path of the binary or script to assess")
 	apipath := flag.String("api-path", "", "[OPTIONAL] The URL path component of the HTTP API to use for peak assessment")
 	apiport := flag.String("api-port", "", "[OPTIONAL] The TCP port of the HTTP API to use for peak assessment")
-	exportfile := flag.String("export-findings", "", "[OPTIONAL] The filesystem path to export findings to; if not provided the results will be written to stdout")
+	exportfile := flag.String("export-findings", "", "[OPTIONAL] The filesystem path to export findings to; if not provided the results will only be written to stdout")
 	flag.Parse()
 	if *target == "" {
 		log.Fatalln("Need at least the target program to proceed")
 	}
 
-	sampletime := 2
-	done := make(chan bool)
-	cmd := exec.Command(*target)
+	// Set up global data structures:
+	isampletime := time.Duration(2) * time.Second
+	psampletime := time.Duration(10) * time.Second
+	idlef = make(chan Findings, 1)
+	peakf = make(chan Findings, 1)
+	icmd = exec.Command(*target)
+	pcmd = exec.Command(*target)
 
-	log.Printf("Launching %v for idle state resource usage assessment", *target)
-	go func() {
-		log.Println("Trying to determine idle state resource usage (no external traffic)")
-		cmd.Run()
-		mem := cmd.ProcessState.SysUsage().(*syscall.Rusage).Maxrss
-		cpuuser := cmd.ProcessState.SysUsage().(*syscall.Rusage).Utime
-		cpusys := cmd.ProcessState.SysUsage().(*syscall.Rusage).Stime
-		log.Printf("Found idle state resource usage. MEMORY: %vkB CPU: %vms (user)/%vms (sys)", mem/1000, cpuuser.Usec/1000, cpusys.Usec/1000)
-		done <- true
-	}()
+	// Perform idle state assessment:
+	go assessidle()
+	<-time.After(isampletime)
+	log.Printf("Idle state assessment of %v completed\n", *target)
+	err := icmd.Process.Signal(os.Interrupt)
+	if err != nil {
+		log.Fatalf("Can't stop process: %v\n", err)
+	}
+	ifs := <-idlef
+	log.Printf("Found idle state resource usage. MEMORY: %vkB CPU: %vms (user)/%vms (sys)",
+		ifs.MemoryMaxRSS/1000,
+		ifs.CPUuser.Usec/1000,
+		ifs.CPUsys.Usec/1000)
 
-	log.Printf("Launching %v for peak state resource usage assessment using 127.0.0.1:%v%v", *target, *apiport, *apipath)
+	// Perform peak state assessment:
+	go assesspeak(*apiport, *apipath)
+	<-time.After(psampletime)
+	log.Printf("Peak state assessment of %v completed\n", *target)
+	err = pcmd.Process.Signal(os.Interrupt)
+	if err != nil {
+		log.Fatalf("Can't stop process: %v\n", err)
+	}
+	pfs := <-peakf
+	log.Printf("Found peak state resource usage. MEMORY: %vkB CPU: %vms (user)/%vms (sys)",
+		pfs.MemoryMaxRSS/1000,
+		pfs.CPUuser.Usec/1000,
+		pfs.CPUsys.Usec/1000)
 
+	// Handle export of findings if instructed to do so
 	if *exportfile != "" {
 		log.Printf("Exporting findings to %v", *exportfile)
 	}
+}
+
+// assessidle performs the idle state resource usage assessment, that is,
+// the memory and CPU usage of the process without external traffic
+func assessidle() {
+	log.Printf("Launching %v for idle state resource usage assessment", icmd.Path)
+	log.Println("Trying to determine idle state resource usage (no external traffic)")
+	icmd.Run()
+	f := Findings{}
+	f.MemoryMaxRSS = icmd.ProcessState.SysUsage().(*syscall.Rusage).Maxrss
+	f.CPUuser = icmd.ProcessState.SysUsage().(*syscall.Rusage).Utime
+	f.CPUsys = icmd.ProcessState.SysUsage().(*syscall.Rusage).Stime
+	idlef <- f
+}
+
+// assesspeak performs the peak state resource usage assessment, that is,
+// the memory and CPU usage of the process with external traffic applied
+func assesspeak(apiport, apipath string) {
+	log.Printf("Launching %v for peak state resource usage assessment", pcmd.Path)
+	log.Printf("Trying to determine peak state resource usage using 127.0.0.1:%v%v", apiport, apipath)
+	go stress(apiport, apipath)
+	pcmd.Run()
+	f := Findings{}
+	f.MemoryMaxRSS = pcmd.ProcessState.SysUsage().(*syscall.Rusage).Maxrss
+	f.CPUuser = pcmd.ProcessState.SysUsage().(*syscall.Rusage).Utime
+	f.CPUsys = pcmd.ProcessState.SysUsage().(*syscall.Rusage).Stime
+	peakf <- f
+}
+
+func stress(apiport, apipath string) {
+	time.Sleep(1 * time.Second)
+	ep := fmt.Sprintf("http://127.0.0.1:%v%v", apiport, apipath)
+	log.Printf("Starting to hammer the endpoint %v", ep)
 	for {
-		select {
-		case <-time.After(time.Duration(sampletime) * time.Second):
-			log.Printf("Sampling completed, stopping %v\n", *target)
-			err := cmd.Process.Signal(os.Interrupt)
-			if err != nil {
-				log.Fatalf("Can't stop process: %v\n", err)
-			}
-		case <-done:
-			return
+		_, err := http.Get(ep)
+		if err != nil {
+			log.Println(err)
 		}
+		time.Sleep(1 * time.Second)
 	}
 }
