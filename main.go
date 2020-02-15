58c08e2309daef1aa836ce1ac1e96af69cf7ee0f
Michael Hausenblas <hausenbl@amazon.com>
2020-02-15 12:39:38 -0800

    completes export
    Signed-off-by: Michael Hausenblas <hausenbl@amazon.com>

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -13,9 +16,9 @@ import (
 
 // Findings captures idle and peak resource usage
 type Findings struct {
-	MemoryMaxRSS int64
-	CPUuser      syscall.Timeval
-	CPUsys       syscall.Timeval
+	MemoryMaxRSS int64 `json:"memory_in_bytes"`
+	CPUuser      int32 `json:"cpuuser_in_usec"`
+	CPUsys       int32 `json:"cpusys_in_usec"`
 }
 
 var icmd, pcmd *exec.Cmd
@@ -26,15 +29,19 @@ func main() {
 	target := flag.String("target", "", "The filesystem path of the binary or script to assess")
 	apipath := flag.String("api-path", "", "[OPTIONAL] The URL path component of the HTTP API to use for peak assessment")
 	apiport := flag.String("api-port", "", "[OPTIONAL] The TCP port of the HTTP API to use for peak assessment")
-	exportfile := flag.String("export-findings", "", "[OPTIONAL] The filesystem path to export findings to; if not provided the results will only be written to stdout")
+	peakdelay := flag.Int("peak-delay", 10, "[OPTIONAL] The time in milliseconds to wait between two consecutive HTTP GET requests")
+	exportfile := flag.String("export-findings", "", "[OPTIONAL] The filesystem path to export findings to; if not provided the results will be written to stdout")
 	flag.Parse()
 	if *target == "" {
 		log.Fatalln("Need at least the target program to proceed")
 	}
 
-	// Set up global data structures:
-	isampletime := time.Duration(1) * time.Second
+	// Set up testing parameters
+	isampletime := time.Duration(2) * time.Second
 	psampletime := time.Duration(5) * time.Second
+	peakhammerpause := time.Duration(*peakdelay) * time.Millisecond
+
+	// Set up global data structures and testing parameter
 	idlef = make(chan Findings, 1)
 	peakf = make(chan Findings, 1)
 	icmd = exec.Command(*target)
@@ -53,11 +60,11 @@ func main() {
 	ifs := <-idlef
 	log.Printf("Found idle state resource usage. MEMORY: %vkB CPU: %vms (user)/%vms (sys)",
 		ifs.MemoryMaxRSS/1000,
-		ifs.CPUuser.Usec/1000,
-		ifs.CPUsys.Usec/1000)
+		ifs.CPUuser/1000,
+		ifs.CPUsys/1000)
 
 	// Perform peak state assessment:
-	go assesspeak(*apiport, *apipath)
+	go assesspeak(*apiport, *apipath, peakhammerpause)
 	<-time.After(psampletime)
 	log.Printf("Peak state assessment of %v completed\n", *target)
 	if pcmd.Process != nil {
@@ -69,13 +76,11 @@ func main() {
 	pfs := <-peakf
 	log.Printf("Found peak state resource usage. MEMORY: %vkB CPU: %vms (user)/%vms (sys)",
 		pfs.MemoryMaxRSS/1000,
-		pfs.CPUuser.Usec/1000,
-		pfs.CPUsys.Usec/1000)
+		pfs.CPUuser/1000,
+		pfs.CPUsys/1000)
 
-	// Handle export of findings if instructed to do so
-	if *exportfile != "" {
-		export(ifs, pfs, *exportfile)
-	}
+	// Handle export of findings:
+	export(ifs, pfs, *exportfile)
 }
 
 // assessidle performs the idle state resource usage assessment, that is,
@@ -87,43 +92,61 @@ func assessidle() {
 	f := Findings{}
 	if icmd.ProcessState != nil {
 		f.MemoryMaxRSS = icmd.ProcessState.SysUsage().(*syscall.Rusage).Maxrss
-		f.CPUuser = icmd.ProcessState.SysUsage().(*syscall.Rusage).Utime
-		f.CPUsys = icmd.ProcessState.SysUsage().(*syscall.Rusage).Stime
+		f.CPUuser = icmd.ProcessState.SysUsage().(*syscall.Rusage).Utime.Usec
+		f.CPUsys = icmd.ProcessState.SysUsage().(*syscall.Rusage).Stime.Usec
 	}
 	idlef <- f
 }
 
 // assesspeak performs the peak state resource usage assessment, that is,
 // the memory and CPU usage of the process with external traffic applied
-func assesspeak(apiport, apipath string) {
+func assesspeak(apiport, apipath string, peakhammerpause time.Duration) {
 	log.Printf("Launching %v for peak state resource usage assessment", pcmd.Path)
 	log.Printf("Trying to determine peak state resource usage using 127.0.0.1:%v%v", apiport, apipath)
-	go stress(apiport, apipath)
+	go stress(apiport, apipath, peakhammerpause)
 	pcmd.Run()
 	f := Findings{}
 	if pcmd.ProcessState != nil {
-		f := Findings{}
 		f.MemoryMaxRSS = pcmd.ProcessState.SysUsage().(*syscall.Rusage).Maxrss
-		f.CPUuser = pcmd.ProcessState.SysUsage().(*syscall.Rusage).Utime
-		f.CPUsys = pcmd.ProcessState.SysUsage().(*syscall.Rusage).Stime
+		f.CPUuser = pcmd.ProcessState.SysUsage().(*syscall.Rusage).Utime.Usec
+		f.CPUsys = pcmd.ProcessState.SysUsage().(*syscall.Rusage).Stime.Usec
 	}
 	peakf <- f
 }
 
-func stress(apiport, apipath string) {
+func stress(apiport, apipath string, peakhammerpause time.Duration) {
 	time.Sleep(1 * time.Second)
 	ep := fmt.Sprintf("http://127.0.0.1:%v%v", apiport, apipath)
-	log.Printf("Starting to hammer the endpoint %v", ep)
+	log.Printf("Starting to hammer the endpoint %v every %v", ep, peakhammerpause)
 	for {
 		_, err := http.Get(ep)
 		if err != nil {
 			log.Println(err)
 		}
-		time.Sleep(1 * time.Second)
+		time.Sleep(peakhammerpause)
 	}
 }
 
 func export(ifs, pfs Findings, exportfile string) {
-	log.Printf("Exporting findings to %v", exportfile)
-	fmt.Printf("%v %v", ifs, pfs)
+	fs := map[string]Findings{
+		"idle": ifs,
+		"peak": pfs,
+	}
+	data, err := json.MarshalIndent(fs, "", " ")
+	if err != nil {
+		log.Printf("Can't serialize findings: %v\n", err)
+	}
+	switch {
+	case exportfile != "": // use export file path provided
+		log.Printf("Exporting findings as JSON to %v", exportfile)
+		err := ioutil.WriteFile(exportfile, data, 0644)
+		if err != nil {
+			log.Printf("Can't export findings: %v\n", err)
+		}
+	default: // if no export file path set, write to stdout
+		_, err := bufio.NewWriter(os.Stdout).Write(data)
+		if err != nil {
+			log.Printf("Can't export findings: %v\n", err)
+		}
+	}
 }
